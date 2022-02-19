a6616711ff17ff0af68146d8c93fbf42bb465252
holgerkurtz <hk@holgerkurtz.de>
2022-02-19 10:49:07 +0100

    changed from gpt-3 to gpt-j

--- poems_ai.py
+++ poems_ai.py
@@ -1,40 +1,77 @@
-# load the API_KEY from the token.json file and set the API_KEY variable
-# https://beta.openai.com/docs/guides/answers
-from dotenv import load_dotenv
+import requests
+from requests.exceptions import HTTPError
+import json
 import os
-import openai
+import logging
+from dotenv import load_dotenv
 
+logging.basicConfig(filename="gpt-j.log",filemode='a', format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 load_dotenv()
 
+class News:
+    """Creates a News and adds context"""
 
-openai.api_key = os.getenv("API_KEY")
-openai.organization = os.getenv("ORGANIZATION")
+    def __init__(self, news: str) -> None:
+        self.news = news
+        self.context = self.get_context()
+
+    def get_context(self):
+        with open("context.txt", "r") as file:
+            context = file.read()
+        input_with_context = context.replace("++NEWS++", self.news)
+        
+        return input_with_context
+
+    def __repr__(self) -> str:
+        return f"{self.context}"
 
 
-def ai_text(news):
-    with open("context.txt", "r") as file:
-        context = file.read()
-    input_with_context = context.replace("++NEWS++", news)
+class TextGen():
+    """Creates a TextGen with parameters via huggingface API"""
+    def __init__(self):
+        self.HUG_API = os.environ.get('HUGGINGFACE_API')
+        self.API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B"
+        self.headers = {"Authorization": f"Bearer {self.HUG_API}"}
+        self.parameters = {
+            "max_new_tokens": 200,
+            "return_full_text": False,
+            "end_sequence" : "---"
+        }
+        self.options = {'use_cache': False}
     
-    multiple_choices = openai.Completion.create(
-        engine="text-davinci-001",
-        temperature=0.9,
-        prompt=input_with_context,
-        stop="\n---\n",
-        n=2,
-        max_tokens=100
-    )
-    print(input_with_context)
-    list_of_choices = []
-    for text in multiple_choices.get('choices'):
-        poem = text.get('text')
-        if poem == "":
-            pass
-        else:
-            list_of_choices.append(poem)
+    def __repr__(self) -> str:
+        return f"{self.HUG_API=} {self.API_URL=} {self.headers=} {self.parameters=} {self.options=}"
     
-    return list_of_choices
-
-
-if __name__ == "__main__":
-    print(ai_text("Paris"))
+    def query(self, news):
+        info = "Es gab ein technisches Problem. Bitte kontaktieren Sie hk@holgerkurtz.de"
+        
+        body = {
+            "inputs": news,
+            "parameters": self.parameters,
+            "options": self.options
+        }
+        response = requests.request(
+            "POST", self.API_URL, headers=self.headers, data=json.dumps(body)
+            )
+        response.raise_for_status()
+        try:
+            if response.status_code == 200:
+                output = response.json()[0]['generated_text']
+                cleaned = output.replace("---", "")
+                return cleaned
+            else:
+                return info
+       
+        except HTTPError as http_err:
+            logger.error(f'HTTP error occurred: {http_err}')  # Python 3.6
+            return info
+      
+        except Exception as err:
+            logger.error(f'Other error occurred: {err}')  # Python 3.6
+            return info
+    
+def ai_text(news_headline):
+    news = News(news_headline).context
+    text = TextGen().query(news)
+    return [text] # return as list because gpt-3 could return multiple texts
