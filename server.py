af712cc70d86532caf54c717d61324b343428c9f
holgerkurtz <hk@holgerkurtz.de>
2022-09-17 18:42:05 +0200

    adding images and news.json to git to access data later

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
-from input_handling import get_news_from_nyt, return_poem_and_image
+# from input_handling import get_news_from_nyt, return_poem_and_image
 import json
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("home.html")
+    with open("news.json", "r") as file:
+        news_list = json.load(file)
+    
+    return render_template("home.html", news_list=news_list)
 
+"""
 @app.route("/<string:nyt_section>", methods=["GET"])
 def section(nyt_section):
     newest_news = get_news_from_nyt(0, nyt_section) # 0 is the first article
@@ -15,13 +19,7 @@ def section(nyt_section):
     news_poems = return_poem_and_image(newest_news.get("title"))
     print(news_poems)
     return render_template("section.html", newest_news=newest_news, list_of_news=news_poems)
-
-@app.route("/archive", methods=["GET"])
-def archive():
-    with open("news.json", "r") as file:
-        news_list = json.load(file)
-    
-    return render_template("archive.html", news_list=news_list)
+"""
 
 if __name__ == "__main__":
     app.run(debug=True)
